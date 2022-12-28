2a4683c7d3f8003a12e4c7ba693ca6c7e31f17a5
Mare Kostic <marko.k0stic@elfak.rs>
2022-12-28 13:18:46 +0100

    Package rename

--- SlackAlertLogger/CdmLoggerConfig.cs
+++ SlackAlertLogger/CdmLoggerConfig.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
-namespace CustomCdmLogger;
+namespace SlackAlertLogger;
 
 public static class CdmLogger
 {
